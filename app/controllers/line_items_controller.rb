69ff58609ee9c6ed42d6813a2659b9866be57b0f
Cori Allen <coriallen@gmail.com>
2013-12-03 15:57:17 -0500

    Create cart view as index of line items.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,4 +3,7 @@ class LineItemsController < ApplicationController
 		LineItem.create(user_id: params[:user_id], product_id: params[:product_id])
 		redirect_to :cart
 	end
+	def index
+		@line_items = LineItem.all
+	end
 end
