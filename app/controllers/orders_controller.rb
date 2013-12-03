10ae9ab018f4e2d24126bf429706538933d78e43
Cori Allen <coriallen@gmail.com>
2013-12-03 16:21:04 -0500

    Working create order method in order controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,17 @@ class OrdersController < ApplicationController
 		### payment processing should happen  in here eventually ###
 
 		# find all the user's current items in 'cart' (line_items)
-
+		cart_items = current_user.line_items.in_cart
+		# find total price of items
+		total_amount = cart_items.sum { |item| item.product.price }
 		# create a new order with a total price from line_items
-
+		order = Order.create(total_price: total_amount)
 		# associate those line items with order
-
+		cart_items.each do |item|
+			item.order = order
+			item.save
+		end
 		# redirect somewhere
-
-
+		redirect_to :root, notice: "checkout complete"
 	end
 end
