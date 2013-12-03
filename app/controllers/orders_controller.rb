d0739c649a40b08c6665e4274e5e85b4630a01be
Cori Allen <coriallen@gmail.com>
2013-12-03 16:11:34 -0500

    Add orders post route and create orders controller update cart view with order button.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+	def create
+
+		### payment processing should happen  in here eventually ###
+
+		# find all the user's current items in 'cart' (line_items)
+
+		# create a new order with a total price from line_items
+
+		# associate those line items with order
+
+		# redirect somewhere
+
+
+	end
+end
