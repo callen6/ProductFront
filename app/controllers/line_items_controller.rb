449024a568b10505d0ee5f887a9063850b193c18
Cori Allen <coriallen@gmail.com>
2013-12-03 15:48:05 -0500

    Update line items controller create method.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,6 @@
 class LineItemsController < ApplicationController
 	def create
-		binding.pry
+		LineItem.create(user_id: params[:user_id], product_id: params[:product_id])
+		redirect_to :cart
 	end
 end
