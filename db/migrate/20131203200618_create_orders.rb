e1dd70f64a6ddd56f909e665d276578d584809d6
Cori Allen <coriallen@gmail.com>
2013-12-03 15:07:11 -0500

    Update order migration.

--- db/migrate/20131203200618_create_orders.rb
+++ db/migrate/20131203200618_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.decimal :total_price
+      t.decimal :total_price, precision: 8,  scale: 2
 
       t.timestamps
     end
